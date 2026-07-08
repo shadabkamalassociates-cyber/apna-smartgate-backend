f364b995dd0149bbc669140cc494876c27be567b
shadabkamalassociates-cyber <shadab.kamalassociates@gmail.com>
2026-07-08 16:44:01 +0000

    ad

--- utils/sendVoipPush.js
+++ utils/sendVoipPush.js
@@ -4,7 +4,7 @@ const apn = require("apn");                // APN library
 async function sendVoipPush(voipToken, callerName, phoneNumber) {
     const note = new apn.Notification();
 
-    note.topic = "com.kamalassociates.aapnasmartgate.voip";
+    note.topic = "com.kamalassociates.aapnasmartgate.dev";
     note.pushType = "voip";
     note.priority = 10;
 
